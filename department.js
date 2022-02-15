42dd64eb7988718babbced21bbbad856c6e50cf7
Mzala123 <justicemwanzamj@gmail.com>
2022-02-15 15:17:14 +0200

    sorting and searching

--- department.js
+++ department.js
@@ -10,8 +10,51 @@ const department = {
     <table class="table table-hover">
   <thead>
     <tr>
-      <th scope="col">Department Id</th>
-      <th scope="col">Department Name</th>
+      <th scope="col">
+
+       <div class="d-flex flex-row">
+       <input  class="form-control m-2"
+         v-model="DepartmentIdFilter"
+         v-on:keyup="FilterFn()"
+         placeholder="filter"/>
+
+         <button type="button" class="btn btn-light"
+         @click="sortResult('DepartmentId',true)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
+         <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+         </svg></button>
+  
+         <button type="button" class="btn btn-light"
+         @click="sortResult('DepartmentId',false)">
+         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+         <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+        </svg>
+         </button>
+
+       </div>
+      
+      Department Id</th>
+      <th scope="col">
+
+      <div class="d-flex flex-row">
+       <input  class="form-control m-2"
+         v-model="DepartmentNameFilter"
+         v-on:keyup="FilterFn()"
+         placeholder="filter"/>   
+
+         <button type="button" class="btn btn-light"
+       @click="sortResult('DepartmentName',true)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
+       <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+       </svg></button>
+
+       <button type="button" class="btn btn-light"
+       @click="sortResult('DepartmentName',false)">
+       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+       <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+      </svg>
+       </button>
+
+       </div>
+      Department Name</th>
       <th scope="col">Options</th>
     
     </tr>
@@ -61,7 +104,6 @@ const department = {
       aria-describedby="basic-addon1" v-model="DepartmentName">
       </div>
 
-      
          <button type="button" @click="createDepartment()"
           v-if="DepartmentId==0" 
           class="btn btn-primary"> 
@@ -88,7 +130,10 @@ const department = {
                departments: [],
                modalTitle: "",
                DepartmentName: "",
-               DepartmentId:0
+               DepartmentId:0,
+               DepartmentNameFilter:"",
+               DepartmentIdFilter:"",
+               departmentsWithoutFilter:[]
             }
         },
         methods: {
@@ -97,6 +142,7 @@ const department = {
                 axios.get(variables.API_URL+"department")
                 .then((response)=>{
                     this.departments = response.data
+                    this.departmentsWithoutFilter = response.data
                     console.log(response.data)
                 })
             },
@@ -137,8 +183,32 @@ const department = {
                    .then((response)=>{
                     this.refreshData();
                     alert(response.data)
-                    })
-                  
+                    })     
+            },
+            FilterFn(){
+              var DepartmentIdFilter = this.DepartmentIdFilter
+              var DepartmentNameFilter = this.DepartmentNameFilter
+              this.departments = this.departmentsWithoutFilter.filter(
+                function(el){
+                  return el.DepartmentId.toString().toLowerCase().includes(
+                    DepartmentIdFilter.toString().trim().toLowerCase()
+ 
+                  )&&
+                  el.DepartmentName.toString().toLowerCase().includes(
+                    DepartmentNameFilter.toString().trim().toLowerCase()
+ 
+                  )
+                }
+              )
+            },
+            sortResult(prop,asc){
+              this.departments = this.departmentsWithoutFilter.sort(function(a,b){
+                if(asc){
+                  return (a[prop] > b[prop])?1:((a[prop]<b[prop])?-1:0)
+                }else{
+                  return (b[prop] > a[prop])?1:((b[prop]<a[prop])?-1:0)
+                }
+              })
             }
         },
         mounted: function(){
